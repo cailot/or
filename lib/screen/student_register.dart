c6c57a2a67b6a099ac26e58f91b93c91d146ad6f
Jinhyung Seo <jin@flutter.com>
2023-03-01 22:54:17 +1100

    basic layout 완성

--- lib/screen/student_register.dart
+++ lib/screen/student_register.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:orca/util/jae_utils.dart';
 import 'package:orca/widget/jae_datepicker.dart';
 
-
 class StudentRegister extends StatefulWidget {
   const StudentRegister({super.key});
 
@@ -16,49 +15,56 @@ class _StudentRegisterState extends State<StudentRegister> {
   @override
   Widget build(BuildContext context) {
     return Container(
-     // color: Colors.amber.shade300,
-     margin: EdgeInsets.all(50),
+      // color: Colors.amber.shade300,
+      margin: EdgeInsets.all(50),
       child: Column(
-
         children: [
           Text(
             'Student Enrolment',
-            style : Theme.of(context).textTheme.labelMedium,
+            style: Theme.of(context).textTheme.labelMedium,
           ),
-          Row(
-            children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 25),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // state
+                renderDropdownList(
+                    label: '',
+                    intialValue: stateDropdownValue,
+                    menus: states,
+                    changed: (String? val) {
+                      setState(() {
+                        stateDropdownValue = val!;
+                      });
+                    }),
+                const SizedBox(
+                  width: 80,
+                ),
+                // branch
+                renderDropdownList(
+                    label: '',
+                    intialValue: branchDropdownValue,
+                    menus: branches,
+                    changed: (String? val) {
+                      setState(() {
+                        branchDropdownValue = val!;
+                      });
+                    }),
+                const SizedBox(
+                  width: 80,
+                ),
 
-              // state 
-              renderDropdownList(
-                label: '',
-                intialValue: stateDropdownValue,
-                menus: states,
-                changed: (String? val){
-                  setState(() {
-                    stateDropdownValue = val!;
-                  });
-                }
-              ),
+                // register button
+                renderButton(label: 'New', tapped: () {}),
+                const SizedBox(
+                  width: 80,
+                ),
 
-              // branch 
-              renderDropdownList(
-                label: '',
-                intialValue: branchDropdownValue,
-                menus: branches,
-                changed: (String? val){
-                  setState(() {
-                    branchDropdownValue = val!;
-                  });
-                }
-              ),
-
-              // register button
-              renderButton(label: 'New', tapped: (){}),
-
-              // save button
-              renderButton(label: 'Save', tapped: (){}),
-
+                // save button
+                renderButton(label: 'Save', tapped: () {}),
               ],
+            ),
           ),
           Container(
             decoration: BoxDecoration(
@@ -74,111 +80,155 @@ class _StudentRegisterState extends State<StudentRegister> {
               key: formKey,
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      renderTextFormField(
-                        label: 'Branch',
-                        onSaved: (val) {},
-                        validator: (val) {
-                          return null;
-                        },
-                      ),
-                      renderTextFormField(
-                        label: 'ID',
-                        onSaved: (val) {},
-                        validator: (val) {
-                          return null;
-                        },
-                      ),
-                      renderTextFormField(
-                        label: 'First Name',
-                        onSaved: (val) {},
-                        validator: (val) {
-                          return null;
-                        },
-                      ),
-                      renderTextFormField(
-                        label: 'Last Name',
-                        onSaved: (val) {},
-                        validator: (val) {
-                          return null;
-                        },
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      top: 30,
+                      bottom: 15,
+                    ),
+                    child: Row(
+                      children: [
+                        renderTextFormField(
+                          label: 'Branch',
+                          onSaved: (val) {},
+                          validator: (val) {
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        renderTextFormField(
+                          label: 'ID',
+                          onSaved: (val) {},
+                          validator: (val) {
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        renderTextFormField(
+                          label: 'First Name',
+                          onSaved: (val) {},
+                          validator: (val) {
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        renderTextFormField(
+                          label: 'Last Name',
+                          onSaved: (val) {},
+                          validator: (val) {
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  Row(
-                    children: [
-                      
-                      //renderDatepicker(),
-                      const Expanded(child: DatePickerTextField()),
-
-
-
-
-
-
-
-                      renderDropdownList(
-                        label: 'Grade',
-                        intialValue: gradeDropdownValue,
-                        menus: grades,
-                        changed: (String? val){
-                          setState(() {
-                            gradeDropdownValue = val!;
-                          });
-                        }
-                      ),
- 
-
-
-                      renderTextFormField(
-                        label: 'Contact No 1',
-                        onSaved: (val) {},
-                        validator: (val) {
-                          return null;
-                        },
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      bottom: 15,
+                      top: 15,
+                    ),
+                    child: Row(
+                      children: [
+                        //renderDatepicker(),
+                        const Expanded(child: DatePickerTextField()),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        renderDropdownList(
+                            label: 'Grade',
+                            intialValue: gradeDropdownValue,
+                            menus: grades,
+                            changed: (String? val) {
+                              setState(() {
+                                gradeDropdownValue = val!;
+                              });
+                            }),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        renderTextFormField(
+                          label: 'Contact No 1',
+                          onSaved: (val) {},
+                          validator: (val) {
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  Row(
-                    children: [
-                      renderTextFormField(
-                        label: 'Contact No 2',
-                        onSaved: (val) {},
-                        validator: (val) {
-                          return null;
-                        },
-                      ),
-                      renderTextFormField(
-                        label: 'Email',
-                        onSaved: (val) {},
-                        validator: (val) {
-                          return null;
-                        },
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      bottom: 15,
+                      top: 15,
+                    ),
+                    child: Row(
+                      children: [
+                        renderTextFormField(
+                          label: 'Contact No 2',
+                          onSaved: (val) {},
+                          validator: (val) {
+                            return null;
+                          },
+                        ),
+                        const SizedBox(width: 30,),
+                        renderTextFormField(
+                          label: 'Email',
+                          onSaved: (val) {},
+                          validator: (val) {
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  Row(
-                    children: [
-                      renderTextFormField(
-                        label: 'Address',
-                        onSaved: (val) {},
-                        validator: (val) {
-                          return null;
-                        },
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      bottom: 15,
+                      top: 15
+                    ),
+                    child: Row(
+                      children: [
+                        renderTextFormField(
+                          label: 'Address',
+                          onSaved: (val) {},
+                          validator: (val) {
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  Row(
-                    children: [
-                      renderTextFormArea(
-                        label: 'Memo',
-                        onSaved: (val) {},
-                        validator: (val) {
-                          return null;
-                        },
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      top: 15,
+                      bottom: 30,
+                    ),
+                    child: Row(
+                      children: [
+                        renderTextFormArea(
+                          label: 'Memo',
+                          onSaved: (val) {},
+                          validator: (val) {
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -209,6 +259,14 @@ class _StudentRegisterState extends State<StudentRegister> {
           ),
           // ignore: sized_box_for_whitespace
           TextFormField(
+            decoration: const InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  width: 1,
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
             onSaved: onSaved,
             validator: validator,
           ),
@@ -237,6 +295,14 @@ class _StudentRegisterState extends State<StudentRegister> {
           ),
           // ignore: sized_box_for_whitespace
           TextFormField(
+            decoration: const InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  width: 1,
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
             maxLines: 3,
             onSaved: onSaved,
             validator: validator,
@@ -245,7 +311,7 @@ class _StudentRegisterState extends State<StudentRegister> {
       ),
     );
   }
-  
+
   renderDropdownList({
     String? label,
     required String intialValue,
@@ -263,9 +329,7 @@ class _StudentRegisterState extends State<StudentRegister> {
             ),
           ),
           InputDecorator(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder()
-            ),
+            decoration: const InputDecoration(border: OutlineInputBorder()),
             child: DropdownButtonHideUnderline(
               child: DropdownButton(
                 value: intialValue,
@@ -284,38 +348,32 @@ class _StudentRegisterState extends State<StudentRegister> {
     );
   }
 
-
-
-
-renderButton({
+  renderButton({
     required String label,
     required Function() tapped,
   }) {
     return ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white, 
-              backgroundColor: Colors.blue,
-              padding : const EdgeInsets.all(25,),
-              elevation: 0.8,
-              textStyle: const TextStyle(
-                fontSize: 22,
-              )
-            ),
-            onPressed: tapped,
-            child: Text(label),
-          );
+      style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.blue,
+          padding: const EdgeInsets.all(
+            25,
+          ),
+          elevation: 0.8,
+          textStyle: const TextStyle(
+            fontSize: 22,
+          )),
+      onPressed: tapped,
+      child: Text(label),
+    );
   }
-
-
-
-
 
   String stateDropdownValue = 'Victoria';
   var states = JaeState.values.map((e) => e.name).toList();
 
   String branchDropdownValue = 'Braybrook';
   var branches = JaeBranch.values.map((e) => e.name).toList();
-  
+
   String gradeDropdownValue = 'P1';
   var grades = JaeGrade.values.map((e) => e.name).toList();
 }
