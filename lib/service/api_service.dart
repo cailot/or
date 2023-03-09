3d45126aa46b84e97579e7ca9ff341cb9f1063b8
Jinhyung Seo <jin@flutter.com>
2023-03-10 09:35:50 +1100

    post api call

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,17 +1,45 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
+import 'package:orca/model/student_model.dart';
+
 class ApiService {
   final String baseUrl = 'http://localhost:8080/student';
 
   final String count = 'count';
 
   void getStudentCount() async {
-    final url = Uri.parse('${baseUrl}s');
+    final url = Uri.parse('$baseUrl/$count');
     final response = await http.get(url);
-    print(response.statusCode);
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       print(response.body);
-    }else{
+    } else {
       throw Error();
+    }
+  }
+
+  Future<StudentModel> addStudent(StudentModel model) async {
+    final url = Uri.parse(baseUrl);
+    final response = await http.post(
+      url,
+      headers: {
+        HttpHeaders.contentTypeHeader: 'application/json',
+      },
+      body: jsonEncode(model.toJson()),
+    );
+
+    print('Response Code : ${response.statusCode} '
+        //with     ${jsonDecode(response.body)}'
+        );
+
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      print('API response: $responseData');
+      return StudentModel.fromJson(responseData);
+    } else {
+      final errorMsg = jsonDecode(response.body)['message'];
+      throw Exception('Failed to register Student');
     }
   }
 }
